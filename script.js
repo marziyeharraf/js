4232f9633b011a269942542afd08a1342f6a23f5
marziye harraf <marziye.hrf@gmail.com>
2022-06-06 14:35:07 +0430

    console log added

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const products = ['html','css','js'];
-const items = products.map(function(product){
-    return `<li>${product}</li>`
-});
-
-const html = items.join(',');
-console.log(html);
+console.log("hello");
